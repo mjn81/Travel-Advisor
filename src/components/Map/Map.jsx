d6ec5db74320bc20f7e28b38577f7fe037dca5db
mjn81 <mjjnn81@gmail.com>
2022-01-31 01:36:58 +0330

    Map Component UI Compelete

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,27 @@
+import GoogleMapReact from "google-map-react";
+import { Paper , Typography , useMediaQuery } from '@material-ui/core';
+import  LocationOnOutlinedIcon  from "@material-ui/icons/LocationCityOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles"
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coords = {lat : 0 , lng:0}
     return ( 
-        <>
-            Map
-        </>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{key : 'AIzaSyBh-p65MnQYbPYno3HNXkclsFE5PQhAPGY'}}
+            defaultCenter={coords}
+            center={coords}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
      );
 }
  
